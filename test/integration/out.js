245bd295d57a1a92362e4f5d7d5e3913fe2305ed
Johan Öbrink <johan.obrink@gmail.com>
2014-04-01 12:26:30 +0200

    Fixed some integration tests

--- test/integration/out.js
+++ test/integration/out.js
@@ -6,13 +6,14 @@ var Wii = require('../../').Wii,
 chai.use(require('sinon-chai'));
 
 describe('MultiWii', function () {
-  var wii;
+  var wii, log;
   before(function (done) {
     wii = new Wii();
 
     wii.on('data', function (data) {
       //console.log(data);
     });
+    log = sinon.spy(); //console.log.bind(log);
 
     Wii.list().then(function (ports) {
       var devices = ports.filter(function (port) {
@@ -34,8 +35,8 @@ describe('MultiWii', function () {
 
   describe('read rc', function () {
     it('can read rc values', function (done) {
-      wii.on('rc', function (data) {
-        console.log(data);
+      wii.once('rc', function (data) {
+        log(data);
         expect(data).to.have.keys(['roll', 'pitch', 'yaw', 'throttle', 'aux1', 'aux2', 'aux3', 'aux4']);
         done();
       });
@@ -45,8 +46,8 @@ describe('MultiWii', function () {
 
   describe('read status', function () {
     it('can read status values', function (done) {
-      wii.on('status', function (data) {
-        console.log(data);
+      wii.once('status', function (data) {
+        log(data);
         expect(data).to.have.keys(['cycleTime', 'i2cErrorCount', 'accBaro', 'dafuq']);
         done();
       });
@@ -56,12 +57,45 @@ describe('MultiWii', function () {
 
   describe('read ident', function () {
     it('can read ident values', function (done) {
-      wii.on('ident', function (data) {
-        console.log(data);
+      wii.once('ident', function (data) {
+        log(data);
         expect(data).to.have.keys(['version', 'multitype', 'subversion']);
         done();
       });
       wii.read('ident');
+    });
+  });
+
+  describe('setRawRc', function () {
+    it('can set rc values', function (done) {
+      var roll = 1200,
+        pitch = 1600,
+        yaw = 700,
+        throttle = 1100,
+        aux1 = 1000,
+        aux2 = 2000,
+        aux3 = 1500,
+        aux4 = 1350;
+
+      wii.once('rc', function (data) {
+        log(data);
+        expect(data).to.eql({
+          roll: roll,
+          pitch: pitch,
+          yaw: yaw,
+          throttle: throttle,
+          aux1: aux1,
+          aux2: aux2,
+          aux3: aux3,
+          aux4: aux4
+        });
+        done();
+      });
+
+      wii
+        .setRawRc(roll, pitch, yaw, throttle, aux1, aux2, aux3, aux4)
+        .then(function () {  wii.read('rc'); })
+        .catch(done);
     });
   });
 });
